94b97c8187a65c336e984bf23a9e8f916a0908dd
Capio21 <130652957+Capio21@users.noreply.github.com>
2023-09-17 08:44:28 +0800

    update

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -11,7 +11,39 @@ class ProductController extends BaseController
 
     public function __construct()
     {
-        $this->product = new ProductModel();
+        $this->product = new \App\Models\ProductModel();
+    }
+
+    public function delete($id)
+    {
+        $this->product->delete($id);
+        return redirect()->to('/product');
+    }
+    public function edit($id)
+    {
+        $data= [
+        'product'=> $this->product->findAll(),
+        'pro' =>$this ->product->where('id',$id)->first(),
+        ];
+        return view('products',$data);
+    }
+
+    public function save()
+    {
+        // Collect data from the request
+        $data = [
+            'UPC' => $this->request->getVar('UPC'),
+            'Product_Name' => $this->request->getVar('Product_Name'),
+            'Price' => $this->request->getVar('Price'),
+            'Quantity' => $this->request->getVar('Quantity'),
+            'Created_at' => $this->request->getVar('Created_at'),
+        ];
+
+        // Save the data to the database using your ProductModel
+        $this->product->save($data);
+
+        // Redirect to the product listing page (adjust the URL as needed)
+        return redirect()->to('/product');
     }
 
     public function product($product)
@@ -21,14 +53,15 @@ class ProductController extends BaseController
 
     public function allan()
     {
-        $data = $this->product->findAll();
-        echo '<pre>';
-        print_r($data);
+        // Fetch all products from the database and pass them to the view
+        $data['product'] = $this->product->findAll();
+        
+        // Load the 'products' view and pass the data to it
+        return view('products', $data);
     }
 
     public function index()
     {
-        //
+        // Your default index action
     }
 }
-
--- app/Views/products.php
+++ app/Views/products.php
@@ -3,9 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Product Listing</title>
 </head>
 <body>
+    <form action="/save" method="post">
+        <label for="UPC">UPC</label>
+        <input type="text" id="UPC" name="UPC" value="">
+        
+        <label for="Product_Name">Product Name</label>
+        <input type="text" id="Product_Name" name="Product_Name" value="">
+        
+        <label for="Price">Price</label>
+        <input type="text" id="Price" name="Price" value="">
+        
+        <label for="Quantity">Quantity</label>
+        <input type="text" id="Quantity" name="Quantity" value="">
+        
+        <label for="Created_at">Created At</label>
+        <input type="datetime-local" id="Created_at" name="Created_at" value="">
+        
+        <input type="submit" name="submit" value="Submit">
+    </form>
     <h1>Product Listing</h1>
+    <table border="1">
+        <tr>
+            <th>UPC</th>
+            <th>Product_Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Created_at</th>
+            <th>Action</th>
+        </tr>
+        <?php foreach($product as $pr): ?>
+            <tr>
+                <td><?= $pr['UPC'] ?></td>
+                <td><?= $pr['Product_Name'] ?></td>
+                <td><?= $pr['Price'] ?></td>
+                <td><?= $pr['Quantity'] ?></td>
+                <td><?= $pr['Created_at'] ?></td>
+                <td>
+                    <a href="/delete/<?= $pr['id'] ?>">Delete</a> || <a href="/edit/<?= $pr['id'] ?>">Edit</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </body>
 </html>

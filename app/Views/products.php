51f8c4733d20959d1209da2827ea4a3b6b7d685b
Capio21 <130652957+Capio21@users.noreply.github.com>
2023-09-17 11:19:07 +0800

    di parin ayos ang update pjhase ko

--- app/Views/products.php
+++ app/Views/products.php
@@ -6,40 +6,61 @@
     <title>Product Listing</title>
     <!-- Include Bootstrap CSS from a CDN (You can also download and host it locally) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
+        /* Centered layout */
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f5f5f5; /* Optional background color */
+        }
+
+        /* Card style */
+        .card {
+            width: 100%;
+            max-width: 800px;
+            padding: 20px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+            background-color: white;
+        }
+    </style>
 </head>
 <body>
-    <div class="container mt-5">
-        <form action="/save" method="post">
-            <div class="mb-3">
-                <label for="UPC" class="form-label">UPC</label>
-                <input type="hidden" name="id" value="<?=$pro['ID'] ?>">
-                <input type="text" class="form-control" id="UPC" name="UPC" placeholder="UPC" value="<?=$pro['UPC'] ?>">
-            </div>
-            
-            <div class="mb-3">
-                <label for="Product_Name" class="form-label">Product Name</label>
-                <input type="text" class="form-control" id="Product_Name" name="Product_Name" placeholder="Product_Name" value="<?=$pro['Product_Name']?>">
-            </div>
-            
-            <div class="mb-3">
-                <label for="Price" class="form-label">Price</label>
-                <input type="text" class="form-control" id="Price" name="Price" placeholder="Price" value="<?=$pro['Price']?>">
-            </div>
-            
-            <div class="mb-3">
-                <label for="Quantity" class="form-label">Quantity</label>
-                <input type="text" class="form-control" id="Quantity" name="Quantity" placeholder="Quantity" value="<?=$pro['Quantity']?>">
-            </div>
-            
-            <div class="mb-3">
-                <label for="Created_at" class="form-label">Created At</label>
-                <input type="datetime-local" class="form-control" id="Created_at" name="Created_at" value="<?=$pro['Created_at']?>">
-            </div>
-            
-            <button type="submit" class="btn btn-primary">Save</button>
-        </form>
+    <div class="card mt-5">
+        <h1 class="mb-4">Product Listing</h1>
+        <div class="center-form">
+            <form action="/save" method="post">
+                <div class="mb-3">
+                    <label for="UPC" class="form-label">UPC</label>
+                    <input type="hidden" name="id" value="<?=$pro['ID'] ?>">
+                    <input type="text" class="form-control" id="UPC" name="UPC" placeholder="UPC" value="<?=$pro['UPC'] ?>">
+                </div>
+
+                <div class="mb-3">
+                    <label for="Product_Name" class="form-label">Product Name</label>
+                    <input type="text" class="form-control" id="Product_Name" name="Product_Name" placeholder="Product_Name" value="<?=$pro['Product_Name']?>">
+                </div>
+
+                <div class="mb-3">
+                    <label for="Price" class="form-label">Price</label>
+                    <input type="text" class="form-control" id="Price" name="Price" placeholder="Price" value="<?=$pro['Price']?>">
+                </div>
+
+                <div class="mb-3">
+                    <label for="Quantity" class="form-label">Quantity</label>
+                    <input type="text" class="form-control" id="Quantity" name="Quantity" placeholder="Quantity" value="<?=$pro['Quantity']?>">
+                </div>
+
+                <div class="mb-3">
+                    <label for="Created_at" class="form-label">Created At</label>
+                    <input type="datetime-local" class="form-control" id="Created_at" name="Created_at" value="<?=$pro['Created_at']?>">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
         
-        <h1 class="mt-5">Product Listing</h1>
         <table class="table table-bordered mt-3">
             <thead>
                 <tr>
